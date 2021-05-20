731ba3feda08a87016edb0112a429bd771945ee2
Unknown <farban6@gmail.com>
2021-05-20 22:10:43 +0100

    Improvements for characters following paths
    Added getPath method in Tilemap which allows us to always get the most updated path. Changed the old getPath method to showPath so its more keeping with its behaviour.

--- node/character.gd
+++ node/character.gd
@@ -15,36 +15,44 @@ var _startingPosition = Vector2()
 var _velocity = Vector2()
 var _characterInfo : CharacterInfo
 var _tileMap : TileMap
+var _pathIndex = 1
+
 
 func _ready():
 	add_to_group("characters")
 	_startingPosition = position
 
+
 func _process(_delta):
 	if _state != States.FOLLOW:
 		return
-	var _arrivedToNextPoint = _move_to(_targetPointWorld)
-	if _arrivedToNextPoint:
-		_path.remove(0)
-		if len(_path) == 0:
+	var path = _tileMap.getPath(_characterInfo)
+	var targetPoint = path[_pathIndex]
+	var arrivedToNextPoint = _move_to(targetPoint)
+	if arrivedToNextPoint:
+		_pathIndex = _pathIndex + 1
+		if _pathIndex == (path.size()):
+			_pathIndex = 1
 			setState(States.IDLE)
-			
 			return
-		_targetPointWorld = _path[0]
 
 
 func setCharacterInfo(characterInfo : CharacterInfo):
 	_characterInfo = characterInfo
 
+
 #TODO this is going to evolve over time. 
 func resetToStartingPosition() -> void:
 	position = _startingPosition
+
 	
 func setTileMap(tileMap : TileMap):
 	_tileMap = tileMap
 
+
 func setState(state):
 	_state = state
+
 	
 func getCharacterInfo():
 	return _characterInfo
@@ -53,7 +61,13 @@ func getCharacterInfo():
 func _unhandled_input(event):
 	if Input.is_action_just_pressed("confirm_click"):
 		_targetPosition = get_global_mouse_position()
-		_change_state(States.IDLE)
+		_characterInfo.setPosition(position)
+		#TODO is this misleading? Its checking for waypoint exists not the path
+		if _tileMap.hasPath(_characterInfo, _targetPosition):
+			return
+		if !_tileMap.isWalkable(_targetPosition):
+			return
+		_tileMap.createPath(_characterInfo, _targetPosition)
 
 
 func _move_to(worldPosition):
@@ -63,24 +77,6 @@ func _move_to(worldPosition):
 	position += _velocity * get_process_delta_time()
 	rotation = _velocity.angle()
 	return position.distance_to(worldPosition) < ARRIVE_DISTANCE
-
-
-func _change_state(state):
-	if state == States.IDLE:
-		_characterInfo.setPosition(position)
-		#TODO is this misleading? Its checking for waypoint exists not the path
-		if _tileMap.hasPath(_characterInfo, _targetPosition):
-			return
-		if !_tileMap.isWalkable(_targetPosition):
-			return
-		_path = _tileMap.createPath(_characterInfo, _targetPosition)
-#		if not _path or len(_path) == 1:
-#			_change_state(States.IDLE)
-#			return
-		# The index 0 is the starting cell.
-		# We don't want the character to move back to it in this example.
-		_targetPointWorld = _path[1]
-	_state = state
 
 
 func _on_Area2D_mouse_entered():
@@ -95,5 +91,5 @@ func _on_Area2D_mouse_exited():
 
 func _on_Area2D_input_event(viewport, event, shape_idx):
 	if event.is_action_pressed("confirm_click"):
-		_tileMap.getPath(_characterInfo)
+		_tileMap.showPath(_characterInfo)
 
